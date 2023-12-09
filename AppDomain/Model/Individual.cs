c1ab60a6421cbb3bed402dbffacfa8d80a890f4d
Olga <Olga@DESKTOP-HI8ST33>
2023-12-09 22:56:11 +0300

    post, get, put, delete added

--- AppDomain/Model/Individual.cs
+++ AppDomain/Model/Individual.cs
@@ -22,40 +22,40 @@ namespace AppDomain.Model
     public partial class DocumentsDto
     {
         public Guid Id { get; set; }
-        public string DocumentName { get; set; }
-        public string DocumentNumber { get; set; }
+        public string DocumentName { get; set; } = "";
+        public string DocumentNumber { get; set; } = "";
     }
 
     public partial class EducationsDto
     {
         public Guid Id { get; set; }
-        public string Type { get; set; }
-        public string Institution { get; set; }
+        public string Type { get; set; } = "";
+        public string Institution { get; set; } = "";
         public DateTime Date { get; set; }
     }
 
     public partial class ForeignPassportDto
     {
         public Guid Id { get; set; }
-        public string DocumentSeries { get; set; }
-        public string DocumentCountry { get; set; }
-        public DateTime DocumentDate { get; set; }
+        public string DocumentSeries { get; set; } = "";
+        public string DocumentCountry { get; set; } = "";
+        public DateTime DocumentDate { get; set; }  
     }
 
     public partial class InformationDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Gender { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string Name { get; set; } = "";
+        public string Gender { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
     }
 
     public partial class InternationalPassportDto
     {
         public Guid Id { get; set; }
-        public string DocumentSeries { get; set; }
-        public string DocumentIssuer { get; set; }
+        public string DocumentSeries { get; set; } = "";
+        public string DocumentIssuer { get; set; } = "";
         public DateTime DocumentDateOfIssue { get; set; }
         public DateTime DocumentExpirationDate { get; set; }
     }
@@ -63,23 +63,23 @@ namespace AppDomain.Model
     public partial class JobsDto
     {
         public Guid Id { get; set; }
-        public string Position { get; set; }
-        public string Organization { get; set; }
+        public string Position { get; set; } = "";
+        public string Organization { get; set; } = "";
     }
 
     public partial class PersonAddressDto
     {
         public Guid Id { get; set; }
-        public string AddressPermanentRegistration { get; set; }
-        public string AddressTemporaryRegistration { get; set; }
+        public string AddressPermanentRegistration { get; set; } = "";
+        public string AddressTemporaryRegistration { get; set; } = "";
     }
 
     public partial class RussianPassportDto
     {
         public Guid Id { get; set; }
-        public string DocumentSeries { get; set; }
-        public string DocumentIssuer { get; set; }
-        public string DocumentIssuerCode { get; set; }
+        public string DocumentSeries { get; set; } = "";
+        public string DocumentIssuer { get; set; } = "";
+        public string DocumentIssuerCode { get; set; } = "";
         public DateTime DocumentDate { get; set; }
     }
 }
